806d401bca68ababdc83244258ac5eda77af0926
Vitor Hugo Brito de Oliveira <hugov1983@gmail.com>
2024-10-03 12:04:36 -0300

    Desabilitando a rotação do LOG para a hospedagem render.com

--- python/my-water-web/logging_config.py
+++ python/my-water-web/logging_config.py
@@ -5,7 +5,7 @@ def setup_logging():
     
     # Configura o logger raiz
     logger = logging.getLogger()
-    logger.setLevel(logging.ERROR)
+    logger.setLevel(logging.INFO)
 
     # Formato dos logs
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -16,6 +16,6 @@ def setup_logging():
     logger.addHandler(console_handler)
 
     # Handler para o arquivo de log
-    file_handler = RotatingFileHandler('logs/app.log', maxBytes=25000, backupCount=5, delay=True, encoding='utf8')
-    file_handler.setFormatter(formatter)
-    logger.addHandler(file_handler)
+    #file_handler = RotatingFileHandler('logs/app.log', maxBytes=25000, backupCount=5, delay=True, encoding='utf8')
+    #file_handler.setFormatter(formatter)
+    #logger.addHandler(file_handler)
